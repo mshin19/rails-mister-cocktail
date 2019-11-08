0023ebed900a6d0259f41a6bc8c54e4be2089537
Miguel Shin <miguelshin@gmail.com>
2019-11-08 17:41:32 -0600

    Complete Cocktails without CSS

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-  before_action :set_cocktail, only: [:show]
+  before_action :set_cocktail, only: [:show, :destroy]
   def index
     @cocktails = Cocktail.all
   end
@@ -18,6 +18,11 @@ class CocktailsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
   end
 
 
